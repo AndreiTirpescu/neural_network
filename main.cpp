a2421c959f78011ff0e70396bc374b76ab8b6af2
AndreiTirpescu <tirpescu.andrei@yahoo.com>
2022-04-01 18:41:40 +0300

    write in one line

--- main.cpp
+++ main.cpp
@@ -4,24 +4,13 @@
 #include "math/matrix.h"
 
 #include "neural_network/network.h"
-#include "math/functions/sigmoid.h"
-#include "math/utils/random_matrix_generator.h"
 
 int main() {
-
     std::shared_ptr<neural_network::network> network = neural_network::network_builder()
-            .withTrainData(
-                    {{1, 1}}, {std::vector<double>{1.0}}
-            )
-            .withTrainData(
-                    {{0, 1}}, {std::vector<double>{1.0}}
-            )
-            .withTrainData(
-                    {{0, 0}}, {std::vector<double>{0.0}}
-            )
-            .withTrainData(
-                    {{1, 0}}, {std::vector<double>{1.0}}
-            )
+            .withTrainData({{1, 1}}, {std::vector<double>{1.0}})
+            .withTrainData({{0, 1}}, {std::vector<double>{1.0}})
+            .withTrainData({{0, 0}}, {std::vector<double>{0.0}})
+            .withTrainData({{1, 0}}, {std::vector<double>{1.0}})
             .withLearningRate(0.03)
             .withRandomWeightsAndSigmoidHiddenLayer(3, 1)
             .build();
